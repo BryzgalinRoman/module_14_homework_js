a02080f715d491a25e4d63578e4022427465fe1e
Roman_Bryzgalin <rbryzgalin@gmail.com>
2023-11-18 20:26:05 +0700

    first commit

---
+++ homework_2.js
@@ -0,0 +1,38 @@
+// Задание 2
+
+// Вам дана заготовка и результат, который вы должны получить. 
+// Ваша задача — написать код, который будет преобразовывать JSON в JS-объект и выводить его в консоль.
+
+const jsonString = `
+{
+    "list": [
+        {
+        "name": "Petr",
+        "age": "20",
+        "prof": "mechanic"
+        },
+
+        {
+        "name": "Vova",
+        "age": "60",
+        "prof": "pilot"
+        }
+    ]
+}`;
+
+const data = JSON.parse(jsonString);
+
+let result = {
+    "list": []
+};
+
+data.list.forEach((item) => {
+    let object = {};
+    object.name = item.name;
+    object.age = item.age;
+    object.prof = item.prof;
+
+    result.list.push(object);
+});
+
+console.log(result);
